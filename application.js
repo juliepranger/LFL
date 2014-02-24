b8d5509d227760b1e4b22634ef9c2635b30e88fe
Julie Pranger <juliepranger@gmail.com>
2014-02-24 09:22:48 -0800

    comments and indentation cleaned up

--- application.js
+++ application.js
@@ -6,9 +6,12 @@ var addresses = ["Times Square, Manhattan, NY 10036",
 								"Stonehenge, A344, Amesbury, Wiltshire SP4 7DE, United Kingdom",
 								"Great Wall of China",
 								"Hollywood Sign, Los Angeles, CA"];
+//lat longs for each of the addresses above
 var coordinates = [];
 var myLatLng;
+//after google geometry calculates the distance between the coordinate pairs listed above and the LFL office
 var distances = [];
+//variable that will allow us to iterate through the locations of each address
 var addCoords;
 
 //let's set Venice, CA to a variable, and set up our map preferences
@@ -31,27 +34,27 @@ $(document).ready(function(myLatLng) {
 	var myLatLng = new google.maps.LatLng(33.988233, -118.459086);
 	// use google geometry library to turn the address into lat and long
 	var getStuff = function(thisAddress){
-			$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+thisAddress+'&sensor=false', null, function (data) {
+		$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+thisAddress+'&sensor=false', null, function (data) {
 		    var p = data.results[0].geometry.location;
 		    //every location, set to a variable name so we can compute the distance between
 		    var addCoords = new google.maps.LatLng(p.lat, p.lng); 
 		    //every location's distance from LFL office computed, converted into kilometers
 				distances.push({ place: thisAddress, distance: (google.maps.geometry.spherical.computeDistanceBetween(myLatLng, addCoords)/1000)});
-		    //sort these distances in numeric, ascending order
+		    //function to sort these distances in numeric, ascending order
 		    var compare = function(a,b) {
-					  if (a.distance < b.distance)
-					     return -1;
-					  if (a.distance > b.distance)
-					    return 1;
-					  return 0;
-					};
+				  if (a.distance < b.distance)
+				     return -1;
+				  if (a.distance > b.distance)
+				    return 1;
+				  return 0;
+				};
 				//set up data visualization, used https://developers.google.com/maps/documentation/javascript/examples/polyline-simple 
 
-				//using addCoords lets us iterate through placeCoords
+				//using addCoords lets us iterate through placeCoords - this is for drawing the polylines between locations
 				var placeCoords = [
 			    myLatLng,
 			    addCoords
-			   ];
+			  ];
 
 			  var journeyPath = new google.maps.Polyline({
 			    path: placeCoords,
@@ -62,7 +65,7 @@ $(document).ready(function(myLatLng) {
 			  });
 			  journeyPath.setMap(map);
 
-				//wait to compare until we put anything to the DOM
+				//wait to compare distances until we put anything to the DOM
 		    if(distances.length == 7) {
 					distances.sort(compare);
 					//now we can use jQuery to put the place name/distance from LFL office to the right div
